0af954c5fc7f52ed04e4e3e8876b59f5812db31a
Jiyeon <ziyeon.ivi@gmail.com>
2022-12-27 02:16:24 +0900

    🚧 Add Link to home and cart page when clicked button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { StaticImage } from 'gatsby-plugin-image';
+import { Link } from 'gatsby';
 
 const HeaderWrapper = styled.header`
   position: fixed;
@@ -53,22 +54,26 @@ export default function Header() {
       </MenuButton>
 
       <LogoWrapper>
-        <StaticImage src="../images/logo.svg" placeholder='none' alt="logo" />
+        <Link to="/">
+          <StaticImage src="../images/logo.svg" placeholder="none" alt="logo" />
+        </Link>
       </LogoWrapper>
 
       <CartButton>
-        <svg
-          className="w-6 h-6"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
-            clipRule="evenodd"
-          ></path>
-        </svg>
+        <Link to="/cart">
+          <svg
+            className="w-6 h-6"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </Link>
       </CartButton>
     </HeaderWrapper>
   );
