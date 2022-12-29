3fca5080303a8aae90b75c768980b897d46ef0dd
Jiyeon <ziyeon.ivi@gmail.com>
2022-12-30 00:29:42 +0900

    🚧 Show real cart item data

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import CartItem from '../components/cartItem';
 import Layout from '../components/layout';
 import OrderBox from '../components/orderBox';
+import { useRecoilState } from 'recoil';
+import { cartItemsState, IProduct } from '../globalState';
 
 const PageWrapper = styled.div`
   @media (${(props) => props.theme.bp.desktop}) {
@@ -11,8 +13,12 @@ const PageWrapper = styled.div`
 `;
 
 const ContentWrapper = styled.div`
-  height: calc(100vh - ${(props) => props.theme.headerHeight.sm});
+  height: calc(100vh - ${(props) => props.theme.headerHeight.sm} - 128px);
   padding: 20px 16px;
+
+  @media (${(props) => props.theme.bp.tablet}) {
+    height: calc(100vh - ${(props) => props.theme.headerHeight.lg});
+  }
 
   @media (${(props) => props.theme.bp.desktop}) {
     flex-basis: 60%;
@@ -21,8 +27,7 @@ const ContentWrapper = styled.div`
 
 const EmptyText = styled.p`
   height: 100%;
-  display: none;
-  /* display: flex; */
+  display: flex;
   justify-content: center;
   align-items: center;
 
@@ -76,32 +81,31 @@ const OrderButton = styled.button`
 
 export default function Cart() {
   const [isOpenOrderBox, setIsOpenOrderBox] = useState(false);
-
+  const [cartItems, setCartItems] = useRecoilState<IProduct[]>(cartItemsState);
+  console.log(cartItems);
   return (
     <Layout isDefaultStyle={true}>
       <PageWrapper>
         <ContentWrapper>
-          <EmptyText>
-            <svg
-              className="w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
-            </svg>
-            장바구니가 비었습니다
-          </EmptyText>
-          <CartList>
-            <CartItem />
-            <CartItem />
-            <CartItem />
-            <CartItem />
-            <CartItem />
-            <CartItem />
-            <CartItem />
-            <CartItem />
-          </CartList>
+          {cartItems.length >= 1 ? (
+            <CartList>
+              {cartItems.map((item) => (
+                <CartItem key={item.id} item={item} />
+              ))}
+            </CartList>
+          ) : (
+            <EmptyText>
+              <svg
+                className="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
+              </svg>
+              장바구니가 비었습니다
+            </EmptyText>
+          )}
         </ContentWrapper>
         <OrderWrapper>
           <TotalPrice>총 결제금액: 123123123</TotalPrice>
