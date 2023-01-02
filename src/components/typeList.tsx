d19bd8f402959612ef906b92a95035ebf597ae65
Jiyeon <ziyeon.ivi@gmail.com>
2023-01-03 03:23:56 +0900

    🚧 Change active's value to string

--- src/components/typeList.tsx
+++ src/components/typeList.tsx
@@ -12,9 +12,9 @@ const ProductTypes = styled.div`
   padding: 32px 12px 0 12px;
 `;
 
-const ProductType = styled(Link)<{ active: boolean }>`
+const ProductType = styled(Link)<{ active: string }>`
   font-size: 14px;
-  font-weight: ${({ active }) => (active ? '800' : '400')};
+  font-weight: ${({ active }) => (active === 'true' ? '800' : '400')};
   margin-right: 8px;
 
   &:last-child {
@@ -32,7 +32,7 @@ export default function TypeList({
       {types.map((type, i) => (
         <ProductType
           to={`/products/${category}?type=${type}`}
-          active={type === selectedType}
+          active={(type === selectedType).toString()}
           key={i}
         >
           {type.toUpperCase()}
