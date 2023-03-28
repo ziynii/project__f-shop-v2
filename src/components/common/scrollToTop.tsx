2cbc8f297db97e932b47678ce0f403f22a264cf0
Jiyeon <ziyeon.ivi@gmail.com>
2023-03-29 03:05:27 +0900

    🐛 Fix the window not defined error

--- src/components/common/scrollToTop.tsx
+++ src/components/common/scrollToTop.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 
 export default function ScrollToTop() {
-  const pathname = window.location.pathname;
+  const pathname = typeof window !== 'undefined' && window.location.pathname;
 
   useEffect(() => {
     window.scrollTo(0, 0);
